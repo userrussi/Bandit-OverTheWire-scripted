b1c16c5b08bb3630b7de915da53dc735ca51927a
userrussi <am4yz@proton.me>
2026-09-11 07:04:09 -0300

    Update bandit21.sh

--- bandit21.sh
+++ bandit21.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat $(cat /usr/bin/cronjob_bandit22.sh | head -n 2 | tail -n 1 | cut -d ' ' -f 3)
+echo "The password is: $(cat $(cat /usr/bin/cronjob_bandit22.sh | head -n 2 | tail -n 1 | cut -d ' ' -f 3))"
