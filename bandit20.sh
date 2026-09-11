339e34ec9cf2bdbf911d3978e5fff325bac8aca0
userrussi <am4yz@proton.me>
2026-09-11 07:02:15 -0300

    Update bandit20.sh

--- bandit20.sh
+++ bandit20.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-(echo "4pIjcunZ0fK2vmp3IwfG8Vf7VhxD6pOA" | nc -l -p 12345) &
+(echo "4pIjcunZ0fK2vmp3IwfG8Vf7VhxD6pOA" | nc -l -p 12345 | echo "The password is: $(head -n 1)") &
 sleep 1
-./suconnect 12345
+./suconnect 12345 2&>/dev/null
